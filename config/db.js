569955ea01ca001ba124b9dff54f6cb03f3f7483
Ishaan-11 <ishaansharma1998@gmail.com>
2021-03-13 13:17:33 +0530

    add: mongo credentiala

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/shortUrlDB", {
+    await mongoose.connect(process.env.MONGO, {
       useNewUrlParser: true, 
       useUnifiedTopology: true
     });
