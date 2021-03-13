5598e85a1781e3eb8d22a4cca31008fd239fff97
Ishaan-11 <ishaansharma1998@gmail.com>
2021-03-13 15:38:01 +0530

    module not issue in heroku

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,6 @@
-const e = require("express");
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/User.js");
 const passport = require('passport');
 
 // @route     GET /login
