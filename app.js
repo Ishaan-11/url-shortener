318fd33570134df85bdc09282fbdd2229b058435
Ishaan-11 <ishaansharma1998@gmail.com>
2021-03-13 13:31:47 +0530

    add Procfile

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.use("/", require("./routes/index"));
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server has started successfully!.");
 });
